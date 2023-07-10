ce2bd03734bb532fea5c88200ad40b5276b6ecc5
sebnow91 <sebnow91@gmail.com>
2023-07-10 11:00:07 +0200

    Add keyboard listener, update README

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Mael
 Mael is a speech-to-text Python bot to assist you in your daily man-to-computer communication. 
 
-## Libraries
+## Dependencies
 To translate speech to text Mael uses module named [SpeechRecognition](https://pypi.org/project/SpeechRecognition/).
 It is required by this module to have [PyAudio](https://pypi.org/project/PyAudio/) installed too.
+To get keyboard input module [pynput](https://pypi.org/project/pynput/) was used.
 To install these modules simply run commands given below:
 `pip install pyaudio`
 `pip install speechrecognition`
+`pip install pynput`
--- main.py
+++ main.py
@@ -2,41 +2,45 @@ import speech_recognition as sr
 from multiprocessing import Process
 from command import process_command
 from notification import send_notification
+from pynput import keyboard
+
 
 def main():
     # Initialize the recognizer
     recognizer = sr.Recognizer()
-    while 1:
 
-        # Exception handling to handle
-        # exceptions at the runtime
-        try:
+    # Exception handling to handle
+    # exceptions at the runtime
+    try:
 
-            # use the microphone as source for input.
-            with sr.Microphone() as source:
+        # use the microphone as source for input.
+        with sr.Microphone() as source:
 
-                # wait for a second to let the recognizer
-                # adjust the energy threshold based on
-                # the surrounding noise level
-                recognizer.adjust_for_ambient_noise(source, duration=0.2)
+            # wait for a second to let the recognizer
+            # adjust the energy threshold based on
+            # the surrounding noise level
+            recognizer.adjust_for_ambient_noise(source, duration=0.2)
 
-                # listens for the user's input
-                send_notification("Enter command/query")
-                audio = recognizer.listen(source)
+            # listens for the user's input
+            send_notification("Enter command/query")
+            audio = recognizer.listen(source)
 
-                # Using google to recognize audio
-                command = recognizer.recognize_google(audio)
-                command = command.lower()
+            # Using google to recognize audio
+            command = recognizer.recognize_google(audio)
+            command = command.lower()
 
-                p = Process(target=process_command, args=(command,))
-                p.start()
+            p = Process(target=process_command, args=(command,))
+            p.start()
 
-        except sr.RequestError as e:
-            send_notification(f"Could not request results; {0}".format(e))
+    except sr.RequestError as e:
+        send_notification(f"Could not request results; {0}".format(e))
 
-        except sr.UnknownValueError:
-            send_notification("unknown error occurred")
+    except sr.UnknownValueError:
+        send_notification("unknown error occurred")
 
 
 if __name__ == '__main__':
-    main()
+    # Listening begins only after pressing a hotkey
+    with keyboard.GlobalHotKeys({
+            '<ctrl>+l': main}) as h:
+        h.join()
